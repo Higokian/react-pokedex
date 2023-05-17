cd76c089c1071ce8b6a4806502e06c11667d26f7
Jason Ritter <jason_ritter@outlook.com>
2023-05-17 18:59:52 -0500

    Test

---
+++ src/Pokecard.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Pokecard extends Component {
+    render() {
+        return (
+            <div>
+                POKECARD!
+            </div>
+        )
+    }
+}
+
+export default Pokecard;
